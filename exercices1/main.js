a66f9d2d0637135413a9b5fe9ffc29ee10b155ba
blanche jose <joseyblanche03@gmail.com>
2024-04-15 17:55:21 -0400

    ajouter fichier js

--- exercices1/main.js
+++ exercices1/main.js
@@ -1,2 +1,6 @@
 /*Changer la couleur de fond d'un élément au survol de la souris :
 ○ Faites en sorte qu'au survol de la souris d'un élément, sa couleur de fond change.*/
+$( "p" )
+  .on( "mouseup", function() {
+    $( this ).append( "<span style='color:#f00;'>Mouse up.</span>" );
+  } );
