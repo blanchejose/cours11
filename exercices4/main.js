367330706f385cd5f9ce0a6c85999e05cd9cf33b
blanche jose <joseyblanche03@gmail.com>
2024-04-21 22:33:09 -0400

    ajouter exercices4

---
+++ exercices4/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    // Afficher une alerte au chargement de la page
+    alert("Bienvenue sur notre site !");
+    // Vous pouvez également utiliser d'autres types de boîtes de dialogue comme des modales avec des bibliothèques comme Bootstrap ou jQuery UI.
+});
