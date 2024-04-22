14e4f2eaa256b66a7c1389a83393ed67bb50b963
blanche jose <joseyblanche03@gmail.com>
2024-04-21 21:28:54 -0400

    ajout exercices2

--- exercices2/exercices2/main.js
+++ exercices2/exercices2/main.js
@@ -4,7 +4,9 @@ const elementToToggle = document.getElementById('elementToToggle');
 toggleButton.addEventListener('click', function() {
     if (elementToToggle.style.display === 'none') {
         elementToToggle.style.display = 'block'; // Afficher l'élément
+        toggleButton.textContent = 'Masquer'; // Changer le texte du bouton
     } else {
         elementToToggle.style.display = 'none'; // Masquer l'élément
+        toggleButton.textContent = 'Afficher'; // Changer le texte du bouton
     }
 });
